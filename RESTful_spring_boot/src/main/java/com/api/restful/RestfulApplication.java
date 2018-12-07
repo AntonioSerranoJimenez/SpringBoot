a624f8fb2d6a5150fcfbef3254bdd2b177121d80
Antonio <Antonio@192.168.1.37>
2018-12-07 09:18:10 +0100

    Added dependencies for running in Tomcat9

--- RESTful_spring_boot/src/main/java/com/api/restful/RestfulApplication.java
+++ RESTful_spring_boot/src/main/java/com/api/restful/RestfulApplication.java
@@ -1,16 +1,31 @@
 package com.api.restful;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestfulApplication {
+public class RestfulApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		
+	/* (non-Javadoc)
+	 * @see org.springframework.boot.web.servlet.support.SpringBootServletInitializer#configure(org.springframework.boot.builder.SpringApplicationBuilder)
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestfulApplication.class);
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 * @throws Exception the exception
+	 */
+	public static void main(String[] args) throws Exception {
+
 		System.out.println("Empezando...");
-		
+
 		SpringApplication.run(RestfulApplication.class, args);
 	}
 }
--- RESTful_spring_boot/src/main/java/com/api/restful/SwaggerConfig.java
+++ RESTful_spring_boot/src/main/java/com/api/restful/SwaggerConfig.java
@@ -4,10 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import com.google.common.base.Predicate;
-
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
